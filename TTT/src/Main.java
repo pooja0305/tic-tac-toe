e96c1b551541fb2017c492abdd596007911112b3
POOJA HARIHAR <88924201+pooja0305@users.noreply.github.com>
2022-08-21 10:32:33 +0530

    Add files via upload

---
+++ TTT/src/Main.java
@@ -0,0 +1,18 @@
+import java.awt.EventQueue;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TicTacToe window = new TicTacToe();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
